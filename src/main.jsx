571380c7ab83ecf75701822d85af65cefa6cf53d
skathirvel059-netizen <skathirvel059@gmail.com>
2026-04-24 17:58:55 +0530

    Configure for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dark-side/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
